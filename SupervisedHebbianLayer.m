bcfa70bdc526f7fd28e3d2234b33b5e6ebc8d13c
ysubbagh <ydns10@gmail.com>
2024-02-08 16:54:22 -0800

    update hebb layer

--- SupervisedHebbianLayer.m
+++ SupervisedHebbianLayer.m
@@ -1,41 +1,85 @@
 classdef SupervisedHebbianLayer
     properties
         weights
-        inputSize
+        inputSize %also output size
         learningRate
+        transferFunction
     end
 
     methods
         %%constructor
-        function this = SupervisedHebbianLayer(inputSize, learningRate)
-            this.inputSize = inputSize;
-            this.learningRate = learningRate;
-            %make weights 0 based on size
-            this.weights = zeros(inputSize);
+        function this = SupervisedHebbianLayer(p1, p2)
+            if(isscalar(p1)) %input size passed
+                this.inputSize = p1;
+                %make weights 0 based on size
+                this.weights = zeros(p1);
+            else %weights passed
+                this.weights = p1;
+                this.inputSize = size(this.weights, 1);
+            end
+            %simplify have learning rule = 1
+            this.learningRate = 1;
+            this.transferFunction = p2;
         end
 
         %%forward function, compute the autoassociator
-        function output = forward(this, inputPattern)
-            output = this.weights * inputPattern';
+        function output = forward(this, input)
+            n = (this.weights * input); 
+            output = arrayfun(@this.doFunc, n);
         end
 
         %%training functions
         %one at a time
-        function this = trainIndividually(this, input, target)
-            weightAdjustment = this.learningRate * (target - this.forward(input)) * input;
-            this.weights = this.weights + weightAdjustment;
+        function new = train(old, input, target)
+            new.weights = old.weights + (target' * input);
         end
 
-        %all at once, using psuedoinverse
-        function this = trainAtOnce(this, input, target)
-            this.weights = target' * pinv(input');
+        function this = pseudoInverseRule(this, input, target)
+            this.weights = target * (inv(input' * input) * input');
+        end
+
+        %---transfer functions---%
+        %transfer function FACTORY
+        function func = doFunc(this, n)
+            switch this.transferFunction
+                case "hardlim"
+                    func = this.hardlim(n);
+                case "hardlims"
+                    func = this.hardlims(n);
+                case "purelin"
+                    func = this.purelin(n);
+                otherwise
+                    error("Transfer function not supported.");
+            end
+        end
+
+        %hardlim
+        function f = hardlim(this, n)
+            if(n < 0)
+                f = 0;
+            else %if n >= 0
+                f = 1;
+            end
+        end
+
+        %hardlimS
+        function f = hardlims(this, n)
+            if(n < 0)
+                f = -1;
+            else %% n >=0
+                f = 1;
+            end
+        end
+        %purelins
+        function f = purelin(this, n)
+            f = n;
         end
 
     end
 
     %%methods outside of the class
     methods (Static)
-        %%addNoise to a vector, distort it
+        %%addNoise to a vector, distort it 
         function pvec = addNoise(pvec, num)
             % ADDNOISE Add noise to "binary" vector
             % pvec pattern vector (-1 and 1)
--- sevenOne.m
+++ sevenOne.m
@@ -1,1 +1,17 @@
-network = SupervisedHebbianLayer(4,1.0);
+%initalize the layer
+network = SupervisedHebbianLayer(4);
+
+%patterns
+p1 = [0 1 0 1];
+p2 = [1 0 1 1];
+p = [p1; p2];
+
+%target
+pt = [1 1 1 1];
+
+disp("weights:" + network.weights);
+
+network = network.train(p, pt);
+
+disp("weights:" + network.weights);
+
